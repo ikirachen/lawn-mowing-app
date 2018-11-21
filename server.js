3fc492c88b287a6dbf356859e21ad4bac3bde990
ikirachen <ikirachen@gmail.com>
2018-11-21 18:10:54 +0200

    Update server.js

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/lawn-mowing'));
+app.use(express.static(__dirname + '/dist/lawn-mowing-app'));
 
 app.get('/*', function(req,res) {
 
-res.sendFile(path.join(__dirname+'/dist/lawn-mowing/index.html'));
+res.sendFile(path.join(__dirname+'/dist/lawn-mowing-app/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
